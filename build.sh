9b5c0937462f3ece14a3fdfb7355a9de82a411f4
ZikraGenexyz <teqi@genexyz.world>
2025-04-14 18:16:50 +0700

    fix: vercel build

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -o errexit
 
 # Install all dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Install SQLite alternative for Vercel
-pip install pysqlite3-binary
+pip3 install pysqlite3-binary
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Uncomment if you need to run migrations during build
 # python manage.py makemigrations
