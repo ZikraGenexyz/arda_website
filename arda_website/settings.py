b176dc71a9b4a62898169c0830987d5c8cec9f9e
ZikraGenexyz <teqi@genexyz.world>
2025-04-14 19:56:11 +0700

    fix: 404 not found

--- arda_website/settings.py
+++ arda_website/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     "arda_app",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
