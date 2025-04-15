d721b1cd9d8e37d9ec56ac316f8dfa62c347dedf
ZikraGenexyz <teqi@genexyz.world>
2025-04-15 10:29:30 +0700

    fix: 404 not found

--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@
 
 set -o errexit
 
-# Install dependencies
 pip3 install -r requirements.txt
 
-# Collect static files
 python3 manage.py collectstatic --noinput
 
-# Make and apply migrations
 python3 manage.py makemigrations
+
 python3 manage.py migrate
